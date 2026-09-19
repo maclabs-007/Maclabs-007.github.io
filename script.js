d305eb79ae2aee823805b393fad92a379c10c221
maclabs-007 <maclabproductions@gmail.com>
2026-09-19 13:39:03 -0400

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const btn=document.querySelector('.menu-toggle'),nav=document.querySelector('nav');btn?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
